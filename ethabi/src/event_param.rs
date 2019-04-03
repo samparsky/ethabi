4bc4b9ebfe24def46d04b1192b7d2af931687281
Ford Nickels <ford@thegraph.com>
2019-04-03 00:48:04 -0700

    Deserialize ABI Tuple types correctly using its components

--- ethabi/src/event_param.rs
+++ ethabi/src/event_param.rs
@@ -1,17 +1,134 @@
 //! Event param specification.
 
-use {ParamType};
+use serde::de::{Error, MapAccess, SeqAccess, Visitor};
+use serde::{Deserialize, Deserializer};
+use std::{fmt, io};
+use ParamType;
+use {errors, Constructor, ErrorKind, Event, Function};
 
 /// Event param specification.
-#[derive(Debug, Clone, PartialEq, Deserialize)]
+#[derive(Debug, Clone, PartialEq)]
 pub struct EventParam {
 	/// Param name.
 	pub name: String,
 	/// Param type.
-	#[serde(rename="type")]
 	pub kind: ParamType,
 	/// Indexed flag. If true, param is used to build block bloom.
 	pub indexed: bool,
+}
+
+pub struct TupleParams {
+    params: Vec<EventParam>,
+}
+
+impl<'a> Deserialize<'a> for TupleParams {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'a>,
+    {
+        deserializer.deserialize_seq(TupleParamsVisitor)
+    }
+}
+
+impl<'a> Deserialize<'a> for EventParam {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'a>,
+    {
+        deserializer.deserialize_any(EventParamVisitor)
+    }
+}
+
+struct EventParamVisitor;
+
+impl<'a> Visitor<'a> for EventParamVisitor {
+    type Value = EventParam;
+
+    fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        write!(formatter, "a valid event parameter spec")
+    }
+
+    fn visit_map<V>(self, mut map: V) -> Result<Self::Value, V::Error>
+    where
+        V: MapAccess<'a>,
+    {
+        let mut name = None;
+        let mut kind = None;
+        let mut indexed = None;
+        let mut components = None;
+
+        while let Some(ref key) = map.next_key::<String>()? {
+            match key.as_ref() {
+                "name" => {
+                    if name.is_some() {
+                        return Err(Error::duplicate_field("name"));
+                    }
+                    name = Some(map.next_value()?);
+                }
+                "type" => {
+                    if kind.is_some() {
+                        return Err(Error::duplicate_field("kind"));
+                    }
+                    kind = Some(map.next_value()?);
+                }
+                "components" => {
+                    if components.is_some() {
+                        return Err(Error::duplicate_field("components"));
+                    }
+                    let component: TupleParams = map.next_value()?;
+                    components = Some(component)
+                }
+                "indexed" => {
+                    if indexed.is_some() {
+                        return Err(Error::duplicate_field("indexed"));
+                    }
+                    indexed = Some(map.next_value()?);
+                }
+                _ => {}
+            }
+        }
+        let name = name.ok_or_else(|| Error::missing_field("name"))?;
+        let kind = kind.ok_or_else(|| Error::missing_field("kind")).map(|t| {
+            if let ParamType::Tuple(_) = t {
+                components.map(|component| component.params).map(|ps| {
+                    ParamType::Tuple(ps.into_iter()
+                        .map(|param| Box::new(param.kind))
+                        .collect::<Vec<Box<ParamType>>>())
+                }).unwrap()
+            } else {
+                t
+            }
+        })?;
+        let indexed = indexed.unwrap_or(false);
+        Ok(EventParam {
+            name,
+            kind,
+            indexed,
+        })
+    }
+}
+
+struct TupleParamsVisitor;
+impl<'a> Visitor<'a> for TupleParamsVisitor {
+    type Value = TupleParams;
+
+    fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        write!(formatter, "a valid event parameter spec")
+    }
+
+    fn visit_seq<A>(self, mut seq: A) -> Result<Self::Value, A::Error>
+    where
+        A: SeqAccess<'a>,
+    {
+        let mut params: Vec<EventParam> = Vec::new();
+
+        while let Some(param) = seq.next_element()? {
+            let p: EventParam = param;
+            params.push(p);
+        }
+
+        Ok(TupleParams { params })
+    }
 }
 
 #[cfg(test)]
@@ -35,4 +152,33 @@ mod tests {
 			indexed: true,
 		});
 	}
+    #[test]
+    fn event_param_tuple_deserialization() {
+        let s = r#"{
+            "name": "foo",
+            "type": "tuple",
+            "indexed": true,
+            "components": [
+                {
+                    "name": "baseToken",
+                    "type": "address"
+                },
+                {
+                    "name": "startDate",
+                    "type": "uint48"
+                }
+            ]
+        }"#;
+
+        let deserialized: EventParam = serde_json::from_str(s).unwrap();
+
+        assert_eq!(
+            deserialized,
+            EventParam {
+                name: "foo".to_owned(),
+                kind: ParamType::Tuple(vec![Box::new(ParamType::Address),Box::new(ParamType::Uint(48))]),
+                indexed: true,
+            }
+        );
+    }
 }
